f5a4d3c4c5e2e0f4eedeb7009ca5574602c6abf9
juliorkm <julioramakrsnamandoju@gmail.com>
2015-06-26 23:13:58 -0300

    Update game.h

--- game.h
+++ game.h
@@ -5,9 +5,9 @@
 #include "player.h"
 #include "projetil.h"
 #include "menu.h"
-#include "mothership.h"
 
 #define N_KEYS 4
+#define FPS 60
 
 enum MYKEYS { KEY_LEFT, KEY_RIGHT, KEY_Z, KEY_ESCAPE };
 
@@ -18,17 +18,15 @@ struct Jogo {
 	ALLEGRO_TIMER* timer;
 	bool key[N_KEYS];
 
+	ALLEGRO_BITMAP* fundo;
+
 	Player player;
-	Mothership mothership;
 	Alien alien[COLUNAS_TROPA][LINHAS_TROPA];
+
 	Menu menu;
 
 	Projetil projetil_stack[50];
 	int numero_de_projeteis;
-
-	int loop_count;
-	int loop_count_projetil;
-	int loop_count_menu;
 };
 
 void inicializa_jogo (Jogo* jogo, int largura, int altura);
@@ -39,7 +37,7 @@ void desenha_jogo (Jogo* jogo);
 
 void loop_de_jogo (Jogo* jogo);
 
-void desenha_fundo_jogo ();
+void desenha_fundo_jogo (Jogo* jogo);
 
 void inicializa_teclado (Jogo* jogo);
 
@@ -47,16 +45,10 @@ void inicializa_event_queue_jogo (Jogo* jogo);
 
 void inicializa_timer_jogo (Jogo* jogo);
 
-void inic_funcoes_allegro (void);
-
 void inic_allegro (void);
 
 void inic_allegro_primitive (void);
 
 void inic_allegro_image (void);
-
-void inic_allegro_font (void);
-
-void inic_allegro_ttf (void);
 
 #endif
